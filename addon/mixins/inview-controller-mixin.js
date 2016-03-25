4511f7e60d69db717794606c87af6c0bd97ac5d6
Remus Rusanu <contact@rusanu.com>
2016-03-25 12:04:51 +0000

    WIP

--- addon/mixins/inview-controller-mixin.js
+++ addon/mixins/inview-controller-mixin.js
@@ -3,12 +3,33 @@ import Ember from 'ember';
 export default Ember.Mixin.create({
   isLoading: false,
   hasMore: true,
-
+  hasError: false,
+  errorMessage: null,
+  modelClass: null,
+  sortProperties: ['numericId:desc'],
+  sortedModel: Ember.computed.sort('model', 'sortProperties'),
+  
   actions: {
     loadMore: function(force) {
-      console.log('inview-controller-mixin: loadMore:', force, 'isLoading:', this.get('isLoading'), 'hasMore:', this.get('hasMore'));
+      if (!force && (!this.get('hasMore') || this.get('hasError'))) {
+        return;
+      }
+      var fromId = this.get('sortedModel.lastObject.numericId');
       this.set('isLoading', true);
-      return true;
-    }
+      this.set('hasError', false);
+      var modelClass = this.get('modelClass');
+      this.store.query(modelClass, {from_id: fromId}).then(
+          function(results) {
+            var count = results.get('length');
+            this.set('hasMore', 0 < count);
+          }.bind(this),
+          function(error) {
+            this.set('hasError', true);
+            this.set('errorMessage', error.message)
+          }.bind(this)
+        ).finally(function () {
+          this.set('isLoading', false);
+        }.bind(this));
+    },
   }
 });
